a7aafde6183e3ea40c3b7f9de26204e71da39d4a
YL0101 <coffee0101@kakao.com>
2022-06-07 14:30:19 +0900

    서버소켓

--- server.py
+++ server.py
@@ -1,1 +1,29 @@
 import socket
+import threading
+import sqlite3
+
+PORT = 9040
+BUF_SIZE = 1024
+lock = threading.Lock()
+
+def DB():
+    conn = sqlite3.connect('db이름.db') # DB 연결
+    cur = conn.cursor()
+    return (conn, cur)
+
+def handle_clnt(clnt_sock):
+
+
+
+
+
+if name == 'main':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', PORT))
+    sock.listen(5)
+
+    while True:
+        clnt_sock, addr = sock.accept()
+
+        thread = threading.Thread(target = handle_clnt, args(clnt_sock,))
+        thread.start()
